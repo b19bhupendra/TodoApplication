00b509004c53723e4ed9440728fe9b039a902ee2
bhupendra <bhupendrayadav2732@gmail.com>
2025-01-15 01:05:23 +0530

    Fixing repo issue

--- todoapp/src/main/java/com/app/todoapp/controller/TaskController.java
+++ todoapp/src/main/java/com/app/todoapp/controller/TaskController.java
@@ -1,25 +1,28 @@
 package com.app.todoapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.todoapp.models.Task;
 import com.app.todoapp.services.ITaskService;
 
 
 @Controller
-@RequestMapping("/tasks")
+@RequestMapping("/")
 public class TaskController {
 	
 	@Autowired
 	private ITaskService taskService;
 	
-	@GetMapping("/getTasks")
+	@GetMapping
 	public String getTasks(Model model) {
 		
-		List<> tasks = taskService.getAllTasks();
+		List<Task> tasks = taskService.getAllTasks();
 		
 		model.addAttribute("tasks",tasks);
 		
--- todoapp/src/main/java/com/app/todoapp/repository/ITaskRepository.java
+++ todoapp/src/main/java/com/app/todoapp/repository/ITaskRepository.java
@@ -1,9 +1,11 @@
 package com.app.todoapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.app.todoapp.models.Task;
 
+@Repository
 public interface ITaskRepository extends JpaRepository<Task, Long> {
 
 }
